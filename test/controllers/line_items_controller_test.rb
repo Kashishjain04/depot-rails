952a993cd9035c021ab9c64c6f5fef9327555e40
Kashish Jain <55389276+Kashishjain04@users.noreply.github.com>
2023-09-06 17:20:12 +0530

    Task E: Smart Cart

--- test/controllers/line_items_controller_test.rb
+++ test/controllers/line_items_controller_test.rb
@@ -2,7 +2,7 @@ require "test_helper"
 
 class LineItemsControllerTest < ActionDispatch::IntegrationTest
   setup do
-    @line_item = line_items(:ruby)
+    @line_item = line_items(:one)
   end
 
   test "should get index" do
@@ -25,8 +25,8 @@ class LineItemsControllerTest < ActionDispatch::IntegrationTest
 
     follow_redirect!
 
-    assert_select 'h2', 'Your Pragmatic Cart'
-    assert_select 'li', 'Programming Ruby 1.9'
+    assert_select 'h2', 'Your Cart'
+    assert_select 'td', "MyString"
   end
 
   test "should show line_item" do
@@ -40,7 +40,8 @@ class LineItemsControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should update line_item" do
-    patch line_item_url(@line_item), params: { line_item: { cart_id: @line_item.cart_id, product_id: @line_item.product_id } }
+    patch line_item_url(@line_item), params: {
+      line_item: { product_id: @line_item.product_id } }
     assert_redirected_to line_item_url(@line_item)
   end
 
