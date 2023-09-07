061af24179332d4ece77cf893bb7a9a355f3443f
Kashish Jain <55389276+Kashishjain04@users.noreply.github.com>
2023-09-07 13:42:03 +0530

    reflect product update in catalog through action cable

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -43,7 +43,7 @@ class ProductsController < ApplicationController
         format.json { render :show, status: :ok, location: @product }
 
         @product.broadcast_replace_later_to 'products',
-                                           partial: 'store/product'
+                                            partial: 'store/product', locals: { updated: true }
       else
         format.html { render :edit, status: :unprocessable_entity }
         format.json { render json: @product.errors, status: :unprocessable_entity }
