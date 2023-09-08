88dbb7950acf100e3af93ef08bedc0d9b5fee55d
Kashish Jain <55389276+Kashishjain04@users.noreply.github.com>
2023-09-08 10:35:26 +0530

    send and test mailer

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,17 @@ require_relative "application"
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+Rails.application.configure do
+  config.action_mailer.delivery_method = :smtp
+
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    name: 'Kashish Jain',
+    authentication: "plain",
+    user_name: "jainabhishek7204@gmail.com",
+    password: "aiZdbZZa",
+    enable_starttls_auto: true
+  }
+end
